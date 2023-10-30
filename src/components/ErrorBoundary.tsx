752555cf8d20eb27d196f163ff8d4aa2cff4b3c4
Aliaksei <sladkiy.aleksey@gmail.com>
2023-10-30 02:51:01 +0100

    feat: add Error Boundary

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import { Component, ReactNode } from 'react';
+
+type ErrorProps = {
+  children: ReactNode;
+};
+
+type ErrorState = {
+  hasError: boolean;
+};
+
+export default class ErrorBoundary extends Component<ErrorProps, ErrorState> {
+  constructor(props: ErrorProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    // eslint-disable-next-line no-console
+    console.log('Произошла ошибка:', error, errorInfo);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+    if (hasError) {
+      return (
+        <div className="error-block">
+          Произошла ошибка. Пожалуйста, обновите страницу.
+        </div>
+      );
+    }
+    return children;
+  }
+}
