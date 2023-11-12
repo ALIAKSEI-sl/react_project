c91d6711b0e37ad6952bf55cc989ffc9fceb4f43
Aliaksei <sladkiy.aleksey@gmail.com>
2023-11-12 23:48:24 +0100

    feat: add test for main page

---
+++ src/app.spec.tsx
@@ -0,0 +1,21 @@
+import { MemoryRouter } from 'react-router-dom';
+
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  it('render elements', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const inputElement = screen.getByRole('textbox') as HTMLInputElement;
+    expect(inputElement).toBeInTheDocument();
+
+    const buttonElement = screen.getByRole('button') as HTMLButtonElement;
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
---
+++ src/pages/MainPage.spec.tsx
@@ -0,0 +1,21 @@
+import { MemoryRouter } from 'react-router-dom';
+
+import { render, screen } from '@testing-library/react';
+
+import MainPage from './MainPage';
+
+describe('MainPage', () => {
+  it('render main page', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <MainPage />
+      </MemoryRouter>
+    );
+
+    const inputElement = screen.getByRole('textbox') as HTMLInputElement;
+    expect(inputElement).toBeInTheDocument();
+
+    const buttonElement = screen.getByRole('button') as HTMLButtonElement;
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
